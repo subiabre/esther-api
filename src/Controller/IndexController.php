37fdde4945a09dcfbac45a8c6dc902e1efb43f66
Daniel Subiabre <subiabrewd@gmail.com>
2024-02-25 18:30:32 +0100

    Add controller at /

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return new RedirectResponse('/api/docs');
+    }
+}
