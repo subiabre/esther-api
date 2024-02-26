c21de7a9c6e7a3d947e1fa0f8b374c34f52c7c98
Daniel Subiabre <subiabrewd@gmail.com>
2024-02-26 18:28:55 +0100

    Update openapi spec

---
+++ src/OpenApi/OpenApiDecorator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\OpenApi;
+
+use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\OpenApi\Model;
+use ApiPlatform\OpenApi\Model\SecurityScheme;
+use ApiPlatform\OpenApi\OpenApi;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+
+#[AsDecorator('api_platform.openapi.factory')]
+class OpenApiDecorator implements OpenApiFactoryInterface
+{
+
+    public function __construct(private OpenApiFactoryInterface $decorated)
+    {
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = $this->decorated->__invoke($context);
+
+        $openApi = $openApi->withServers([
+            new Model\Server('/'),
+            new Model\Server('http://{{api}}/v1')
+        ]);
+
+        $securitySchemes = $openApi->getComponents()->getSecuritySchemes() ?: new \ArrayObject();
+        $securitySchemes['access_token'] = new SecurityScheme(
+            type: 'http',
+            scheme: 'bearer',
+        );
+
+        return $openApi;
+    }
+}
