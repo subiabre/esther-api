c68dfb488a9da333b70ca25c31d32d3ee93cd2b5
Daniel Subiabre <subiabrewd@gmail.com>
2024-03-18 20:09:16 +0100

    Update ImageFile constraint message

--- src/Validator/ImageFile.php
+++ src/Validator/ImageFile.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\Constraint;
 #[\Attribute]
 class ImageFile extends Constraint
 {
-    public string $message = 'The resource at "{{ string }}" is not a valid image: it can only be a JPEG, GIF or PNG file.';
+    public string $message = 'The resource at "{{ string }}" is not a valid image.';
 
     public function __construct(string $message = null, array $groups = null, $payload = null)
     {
