ac07b2a366692d2808baab5f8c1055f2199270aa
Daniel Subiabre <subiabrewd@gmail.com>
2024-06-30 17:57:31 +0200

    Add back dependency on stof/doctrine-extensions-bundle

--- src/Entity/Trait/TimestampableCreation.php
+++ src/Entity/Trait/TimestampableCreation.php
@@ -5,9 +5,11 @@ namespace App\Entity\Trait;
 use ApiPlatform\Metadata as API;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\Timestampable;
 
 trait TimestampableCreation
 {
+    #[Timestampable(on: 'create')]
     #[API\ApiProperty(writable: false)]
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
     protected $dateCreated;
--- src/Entity/Trait/TimestampableUpdation.php
+++ src/Entity/Trait/TimestampableUpdation.php
@@ -5,9 +5,11 @@ namespace App\Entity\Trait;
 use ApiPlatform\Metadata as API;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\Timestampable;
 
 trait TimestampableUpdation
 {
+    #[Timestampable(on: 'update')]
     #[API\ApiProperty(writable: false)]
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
     protected $dateUpdated;
