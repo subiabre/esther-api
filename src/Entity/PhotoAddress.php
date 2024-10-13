2083c3ef8e04f7e8a23429103e6f53a83818568f
Daniel Subiabre <subiabrewd@gmail.com>
2024-10-13 21:07:44 +0200

    Fix photo address not being logged

--- src/Entity/PhotoAddress.php
+++ src/Entity/PhotoAddress.php
@@ -4,21 +4,27 @@ namespace App\Entity;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 #[ORM\Embeddable]
+#[Gedmo\Loggable()]
 class PhotoAddress
 {
     public function __construct(
         #[ORM\Column(type: Types::STRING, nullable: true)]
+        #[Gedmo\Versioned()]
         public ?string $fullName,
 
         #[ORM\Column(type: Types::STRING, nullable: true)]
+        #[Gedmo\Versioned()]
         public ?string $shortName,
 
         #[ORM\Column(type: Types::JSON, nullable: true)]
+        #[Gedmo\Versioned()]
         public ?array $components,
 
         #[ORM\Column(type: Types::STRING, nullable: true)]
+        #[Gedmo\Versioned()]
         public ?string $reference,
     ) {
     }
